b7b4f1ff7f6dda3469e347c08c6eee38d9df090a
Tuuz <silverhawk@foxmail.com>
2021-02-04 21:31:49 +0800

    TuuzPC

--- lib/tuuz/toasts/toast.dart
+++ lib/tuuz/toasts/toast.dart
@@ -5,12 +5,13 @@ class Toasts {
   Future<void> Show(String text) {
     Fluttertoast.showToast(
       msg: text,
-      toastLength: Toast.LENGTH_LONG,
-      gravity: ToastGravity.CENTER,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
       timeInSecForIosWeb: 1,
-      backgroundColor: Colors.red,
+      backgroundColor: Colors.black87,
       textColor: Colors.white,
       fontSize: 16.0,
     );
   }
+
 }
