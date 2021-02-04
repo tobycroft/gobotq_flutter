b5584a79ca2f29f04dbf67aec112ef922db2fbd1
Tuuz <silverhawk@foxmail.com>
2021-02-04 13:54:44 +0800

    TuuzPC

--- lib/tuuz/net/ret.dart
+++ lib/tuuz/net/ret.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:gobotq_flutter/tuuz/alert/ios.dart';
 
 class Ret {
-  bool Check(BuildContext context, Map json) {
+  bool Check_isok(BuildContext context, Map json) {
     if (json["code"] != 0) {
       Alert().Confirm(context, "错误提示", json["echo"], () {});
       return false;
