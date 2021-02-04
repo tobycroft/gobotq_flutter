0a49f6409e241645035d32ba2e2b1261f4a51ab6
Tuuz <silverhawk@foxmail.com>
2021-02-04 13:41:34 +0800

    TuuzPC

---
+++ lib/app/index2/url_index2.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:gobotq_flutter/config/url.dart';
+
+class Url_Index2 extends Url {
+  String Group_list_control = "/v1/group/list/control";
+  String Group_list_joined = "/v1/group/list/joined";
+}
