1cf4e8fa31e727790c5053f10d519701067c1460
Tuuz <silverhawk@foxmail.com>
2021-02-08 01:15:38 +0800

    TuuzPC

---
+++ lib/app/index1/robot_info/white/url_white.dart
@@ -0,0 +1,12 @@
+import 'package:gobotq_flutter/app/index1/robot_info/url_robot_info.dart';
+
+class Url_white extends Url_robot_info {
+
+  String white_list = "/v1/bot/group/white_list";
+  String white_add = "/v1/bot/group/white_add";
+  String white_delete = "/v1/bot/group/white_delete";
+
+  String group_list = "/v1/bot/group/group_list";
+  String group_add = "/v1/bot/group/group_add";
+  String group_exit = "/v1/bot/group/group_exit";
+}
