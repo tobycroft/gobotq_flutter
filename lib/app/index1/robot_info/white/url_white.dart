e20d171f233177c3e7c5f4a9bc6130a659ae3e05
Tuuz <silverhawk@foxmail.com>
2021-02-08 01:17:02 +0800

    TuuzPC

--- lib/app/index1/robot_info/white/url_white.dart
+++ lib/app/index1/robot_info/white/url_white.dart
@@ -1,9 +1,7 @@
 import 'package:gobotq_flutter/app/index1/robot_info/url_robot_info.dart';
 
 class Url_white extends Url_robot_info {
-
   String white_list = "/v1/bot/group/white_list";
   String white_add = "/v1/bot/group/white_add";
   String white_delete = "/v1/bot/group/white_delete";
-
 }
