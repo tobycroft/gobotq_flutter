45760ef1ccd2a6c62b705ae217b597686edc25b1
Tuuz <silverhawk@foxmail.com>
2021-02-08 11:54:31 +0800

    TuuzPC

---
+++ lib/app/index1/robot_info/list/bot_group_list.dart
@@ -0,0 +1,187 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_easyrefresh/easy_refresh.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:gobotq_flutter/app/index1/robot_info/list/url_list.dart';
+import 'package:gobotq_flutter/app/index1/robot_info/white/bot_white_list_add.dart';
+import 'package:gobotq_flutter/config/auth.dart';
+import 'package:gobotq_flutter/config/config.dart';
+import 'package:gobotq_flutter/extend/authaction/authaction.dart';
+import 'package:gobotq_flutter/tuuz/alert/ios.dart';
+import 'package:gobotq_flutter/tuuz/net/net.dart';
+import 'package:gobotq_flutter/tuuz/net/ret.dart';
+import 'package:gobotq_flutter/tuuz/win/close.dart';
+
+class BotGroupList extends StatefulWidget {
+  String _title;
+  var _pageparam;
+
+  BotGroupList(this._title, this._pageparam);
+
+  _BotGroupList createState() => _BotGroupList(this._title, this._pageparam);
+}
+
+class _BotGroupList extends State<BotGroupList> {
+  String _title;
+  var _pageparam;
+
+  _BotGroupList(this._title, this._pageparam);
+
+  @override
+  void initState() {
+    setState(() {
+      get_data(context, this._pageparam["bot"].toString());
+    });
+    super.initState();
+  }
+
+  Future<void> get_data(BuildContext context, String bot) async {
+    Map post = await AuthAction().LoginObject();
+    post["bot"] = bot;
+
+    String ret = await Net().Post(Config().Url, Url_List().group_list, null, post, null);
+    Map json = jsonDecode(ret);
+    if (Auth().Return_login_check(context, json)) {
+      if (Ret().Check_isok(context, json)) {
+        _white_group = json["data"];
+        setState(() {
+          _white_group = json["data"];
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          this._title,
+          style: Config().Text_style_title,
+        ),
+        actions: [
+          FlatButton(
+            onPressed: () async {
+              Windows().Open(context, Bot_white_list_add(this._title, this._pageparam));
+            },
+            child: Icon(
+              Icons.add_circle_outline,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      body: EasyRefresh(
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            String groupname = "";
+            var _data = _white_group[index];
+            if (_data["group_info"] != null) {
+              groupname = _data["group_info"]["group_name"].toString();
+            } else {
+              groupname = "暂时还未拉入这个群";
+            }
+
+            return new Slidable(
+              actionPane: SlidableScrollActionPane(),
+              //滑出选项的面板 动画
+              actionExtentRatio: 0.25,
+              child: ListTile(
+                leading: null,
+                title: Text(groupname),
+                subtitle: Text(_data["gid"].toString()),
+                trailing: null,
+                onTap: () async {},
+              ),
+              secondaryActions: [
+                IconSlideAction(
+                  caption: '删除',
+                  color: Colors.red,
+                  icon: Icons.delete_forever,
+                  onTap: () async {
+                    bool ret = await delete_data(context, _data["bot"].toString(), _data["gid"].toString());
+                    if (ret) {
+                      setState(() {
+                        _white_group.removeAt(index);
+                      });
+                    }
+                  },
+                )
+              ],
+            );
+          },
+          itemCount: _white_group.length,
+        ),
+        onRefresh: () async {
+          get_data(context, this._pageparam["bot"].toString());
+        },
+      ),
+    );
+  }
+}
+
+Future<bool> delete_data(BuildContext context, String bot, gid) async {
+  Map post = await AuthAction().LoginObject();
+  post["bot"] = bot;
+  post["gid"] = gid;
+
+  String ret = await Net().Post(Config().Url, Url_List().group_exit, null, post, null);
+  Map json = jsonDecode(ret);
+  if (Auth().Return_login_check(context, json)) {
+    if (Ret().Check_isok(context, json)) {
+      Alert().Confirm(context, json["echo"], json["echo"], () {});
+      return true;
+    }
+  }
+  return false;
+}
+
+List _white_group = [];
+
+// class _list_builder extends State {
+//   BuildContext context;
+//   var _index;
+//   var _data;
+//
+//   _list_builder(this.context, this._index, this._data);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     String groupname = "";
+//     if (this._data["group_info"] != null) {
+//       groupname = this._data["group_info"]["group_name"].toString();
+//     } else {
+//       groupname = "暂时还未拉入这个群";
+//     }
+//
+//     return Slidable(
+//       actionPane: SlidableScrollActionPane(),
+//       //滑出选项的面板 动画
+//       actionExtentRatio: 0.25,
+//       key: Key(this._index.toString()),
+//       child: ListTile(
+//         leading: null,
+//         title: Text(groupname),
+//         subtitle: Text(this._data["gid"].toString()),
+//         trailing: null,
+//         onTap: () async {},
+//       ),
+//       secondaryActions: [
+//         IconSlideAction(
+//           caption: '删除',
+//           color: Colors.red,
+//           icon: Icons.delete_forever,
+//           onTap: () async {
+//             _white_group.removeAt(this._index);
+//
+//             bool ret = await delete_data(context, this._data["bot"].toString(), this._data["gid"].toString());
+//             if (ret) {
+//               setState(() {});
+//             }
+//           },
+//         )
+//       ],
+//     );
+//   }
+// }
---
+++ lib/app/index1/robot_info/list/bot_white_list_add.dart
@@ -0,0 +1,93 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gobotq_flutter/app/index1/robot_info/list/url_list.dart';
+import 'package:gobotq_flutter/config/auth.dart';
+import 'package:gobotq_flutter/config/config.dart';
+import 'package:gobotq_flutter/extend/authaction/authaction.dart';
+import 'package:gobotq_flutter/tuuz/alert/ios.dart';
+import 'package:gobotq_flutter/tuuz/net/net.dart';
+import 'package:gobotq_flutter/tuuz/net/ret.dart';
+import 'package:gobotq_flutter/tuuz/ui/ui_button.dart';
+import 'package:gobotq_flutter/tuuz/win/close.dart';
+
+class Bot_group_list_add extends StatefulWidget {
+  String _title;
+  var _pageparam;
+
+  Bot_group_list_add(this._title, this._pageparam);
+
+  _Bot_group_list_add createState() => _Bot_group_list_add(this._title, this._pageparam);
+}
+
+class _Bot_group_list_add extends State<Bot_group_list_add> {
+  String _title;
+  var _pageparam;
+
+  _Bot_group_list_add(this._title, this._pageparam);
+
+  String gid;
+  bool _gid = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          this._title,
+          style: Config().Text_style_title,
+        ),
+      ),
+      body: ListView(
+        padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+        children: [
+          Container(
+            child: Text("这里填写机器人可以加入的群"),
+          ),
+          Container(
+            child: Text("如果不填写，机器人将无法拉入这个群"),
+          ),
+          Container(
+            child: Text("机器人会定期退群，机器人将会自动退出未加入白名单的群"),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            style: Theme.of(context).textTheme.headline4,
+            decoration: Config().Inputdecoration_default_input_box(Icons.account_circle, "输入机器人可以加入的群", this._gid, "请输入数字"),
+            onChanged: (String val) {
+              setState(() {
+                if (int.tryParse(val) == null) {
+                  this._gid = true;
+                } else {
+                  this._gid = false;
+                }
+              });
+              this.gid = val.toString();
+            },
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          UI_button().Button_submit(context, () async {
+            Map post = await AuthAction().LoginObject();
+            post["gid"] = this.gid.toString();
+            post["bot"] = this._pageparam["bot"].toString();
+            String ret = await Net().Post(Config().Url, Url_List().group_add, null, post, null);
+            Map json = jsonDecode(ret);
+            if (Auth().Return_login_check(context, json)) {
+              if (Ret().Check_isok(context, json)) {
+                Alert().Confirm(context, json["echo"], json["echo"], () {
+                  Windows().Close(this.context);
+                });
+              }
+            }
+          }),
+        ],
+      ),
+    );
+  }
+}
