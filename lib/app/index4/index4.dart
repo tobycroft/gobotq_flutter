89cbe1d0cf2667ca0d2f81ced859cbeec8211041
Tuuz <silverhawk@foxmail.com>
2021-04-11 22:01:32 +0800

    TuuzPC

--- lib/app/index4/index4.dart
+++ lib/app/index4/index4.dart
@@ -99,6 +99,51 @@ class _Index4 extends State<Index4> {
     }
   }
 
+  Future<void> update_app() async {
+    PackageInfo info = await PackageInfo.fromPlatform();
+    int version_code = int.tryParse(info.buildNumber);
+    Map<String, String> post = {
+      "platform": Platform.operatingSystem.toString(),
+      "dart": Platform.version.toString(),
+      "system": Platform.operatingSystemVersion.toString(),
+      "version": info.version,
+      "version_code": version_code.toString(),
+      "package_name": info.packageName,
+      "appname": info.appName,
+    };
+
+    String ret = await Net.Post(Config.Url, Url.Update_path, null, post, null);
+    Map json = jsonDecode(ret);
+    if (json["code"] == 0) {
+      Map data = json["data"];
+
+      if (version_code < int.parse(data["version"])) {
+        String upgrade_text = "";
+        upgrade_text += "当权版本：";
+        upgrade_text += info.version.toString() + "\r\n";
+        upgrade_text += "有新版本：";
+        upgrade_text += data["show_ver"].toString() + "\r\n";
+
+        upgrade_text += "更新内容：\r\n";
+        upgrade_text += data["upgrade_text"].toString() + "\r\n";
+
+        upgrade_text += "更新日期:" + data["date"].toString();
+
+        Alert.Simple(context, "有新的更新", upgrade_text, () {
+          RUpgrade.upgrade(
+            data["url"].toString(),
+            fileName: data["file"].toString(),
+            isAutoRequestInstall: true,
+            notificationStyle: NotificationStyle.speechAndPlanTime,
+            useDownloadManager: false,
+          );
+        });
+      } else {
+        Alert.Confirm(context, "没有新的更新了", "", () {});
+      }
+    }
+  }
+
   Map _user_info = {
     "uname": "请先登录",
     "qq": "",
@@ -227,48 +272,7 @@ class _Index4 extends State<Index4> {
                     ],
                   ),
                   onPressed: () async {
-                    PackageInfo info = await PackageInfo.fromPlatform();
-                    int version_code = int.tryParse(info.buildNumber);
-                    Map<String, String> post = {
-                      "platform": Platform.operatingSystem.toString(),
-                      "dart": Platform.version.toString(),
-                      "system": Platform.operatingSystemVersion.toString(),
-                      "version": info.version,
-                      "version_code": version_code.toString(),
-                      "package_name": info.packageName,
-                      "appname": info.appName,
-                    };
-
-                    String ret = await Net.Post(Config.Url, Url.Update_path, null, post, null);
-                    Map json = jsonDecode(ret);
-                    if (json["code"] == 0) {
-                      Map data = json["data"];
-
-                      if (version_code < int.parse(data["version"])) {
-                        String upgrade_text = "";
-                        upgrade_text += "当权版本：";
-                        upgrade_text += info.version.toString() + "\r\n";
-                        upgrade_text += "有新版本：";
-                        upgrade_text += data["show_ver"].toString() + "\r\n";
-
-                        upgrade_text += "更新内容：\r\n";
-                        upgrade_text += data["upgrade_text"].toString() + "\r\n";
-
-                        upgrade_text += "更新日期:" + data["date"].toString();
-
-                        Alert.Simple(context, "有新的更新", upgrade_text, () {
-                          RUpgrade.upgrade(
-                            data["url"].toString(),
-                            fileName: data["file"].toString(),
-                            isAutoRequestInstall: true,
-                            notificationStyle: NotificationStyle.speechAndPlanTime,
-                            useDownloadManager: false,
-                          );
-                        });
-                      } else {
-                        Alert.Confirm(context, "没有新的更新了", "", () {});
-                      }
-                    }
+                    await update_app();
                   },
                 ),
                 FlatButton(
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:event_hub/event_hub.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -5,8 +8,15 @@ import 'package:gobotq_flutter/app/index1/index1.dart';
 import 'package:gobotq_flutter/app/index2/index2.dart';
 import 'package:gobotq_flutter/app/index3/index3.dart';
 import 'package:gobotq_flutter/app/index4/index4.dart';
+import 'package:gobotq_flutter/config/config.dart';
+import 'package:gobotq_flutter/config/url.dart';
+import 'package:gobotq_flutter/tuuz/alert/ios.dart';
+import 'package:gobotq_flutter/tuuz/net/net.dart';
 import 'package:jpush_flutter/jpush_flutter.dart';
-import 'package:gobotq_flutter/config/style.dart';
+import 'package:gobotq_flutter/config/style.dart' as st;
+import 'package:package_info/package_info.dart';
+import 'package:path/path.dart';
+import 'package:r_upgrade/r_upgrade.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,10 +27,52 @@ final JPush jpush = new JPush();
 final EventHub eventhub = EventHub();
 
 class Init {
-  void init() async {
+  void init(BuildContext context) async {
     // PackageInfo packageInfo = await PackageInfo.fromPlatform();
     // print(packageInfo.appName);
     // print(packageInfo.version);
+      PackageInfo info = await PackageInfo.fromPlatform();
+      int version_code = int.tryParse(info.buildNumber);
+      Map<String, String> post = {
+        "platform": Platform.operatingSystem.toString(),
+        "dart": Platform.version.toString(),
+        "system": Platform.operatingSystemVersion.toString(),
+        "version": info.version,
+        "version_code": version_code.toString(),
+        "package_name": info.packageName,
+        "appname": info.appName,
+      };
+
+      String ret = await Net.Post(Config.Url, Url.Update_path, null, post, null);
+      Map json = jsonDecode(ret);
+      if (json["code"] == 0) {
+        Map data = json["data"];
+
+        if (version_code < int.parse(data["version"])) {
+          String upgrade_text = "";
+          upgrade_text += "当权版本：";
+          upgrade_text += info.version.toString() + "\r\n";
+          upgrade_text += "有新版本：";
+          upgrade_text += data["show_ver"].toString() + "\r\n";
+
+          upgrade_text += "更新内容：\r\n";
+          upgrade_text += data["upgrade_text"].toString() + "\r\n";
+
+          upgrade_text += "更新日期:" + data["date"].toString();
+
+          Alert.Simple(context, "有新的更新", upgrade_text, () {
+            RUpgrade.upgrade(
+              data["url"].toString(),
+              fileName: data["file"].toString(),
+              isAutoRequestInstall: true,
+              notificationStyle: NotificationStyle.speechAndPlanTime,
+              useDownloadManager: false,
+            );
+          });
+        } else {
+          // Alert.Confirm(context, "没有新的更新了", "", () {});
+        }
+      }
   }
 
   void is_login() async {}
@@ -44,7 +96,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         // primaryColor: Colors.grey,
         brightness: Brightness.light,
-        scaffoldBackgroundColor: Style.LightGrey,
+        scaffoldBackgroundColor: st.Style.LightGrey,
         appBarTheme: AppBarTheme(
           brightness: Brightness.light,
           color: Colors.white54,
@@ -62,9 +114,9 @@ class MyApp extends StatelessWidget {
         iconTheme: IconThemeData(color: Colors.black),
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
-        bottomNavigationBarTheme: BottomNavigationBarThemeData(backgroundColor: Style.LightGrey),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(backgroundColor: st.Style.LightGrey),
         dividerTheme: DividerThemeData(
-          color: Style.LightGrey,
+          color: st.Style.LightGrey,
           thickness: 0.3,
           space: 0.5,
         ),
@@ -101,7 +153,7 @@ class BotomeMenumPageState extends State<BotomeMenumPage> {
     ///初始化，这个函数在生命周期中只调用一次
     initPlatformState();
 
-    Init().init();
+    Init().init(this.context);
 
     super.initState();
   }
