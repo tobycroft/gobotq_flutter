2356160d5b20427df933efc90ee82fff0398fbf1
Tuuz <miracle-plus@outlook.com>
2021-05-05 16:26:11 +0800

    TuuzMac

--- lib/app/index3/upload_server/url_upload_server.dart
+++ lib/app/index3/upload_server/url_upload_server.dart
@@ -1,6 +1,6 @@
 import 'package:gobotq_flutter/config/url.dart';
 
 class Url_upload_server extends Url {
-  static String Upload_server = "/v1/bot/bot/add";
-  static String Upload_list = "/v1/bot/bot/list";
+  static String Upload_server = "/v1/bot/server/add";
+  static String Upload_list = "/v1/bot/server/list";
 }
--- lib/config/config.dart
+++ lib/config/config.dart
@@ -58,13 +58,13 @@ class Config {
     );
   }
 
-  static String ProxyURL = "10.0.0.100:9000";
+  static String ProxyURL = "192.168.1.10:9000";
 
-  // static bool Proxy_debug = true;
-  // static String Url = "127.0.0.1";
+  static bool Proxy_debug = true;
+  static String Url = "http://192.168.1.10:81";
 
-  static bool Proxy_debug = false;
-  static String Url = "http://api.tuuz.cc:15081";
+  // static bool Proxy_debug = false;
+  // static String Url = "http://api.tuuz.cc:15081";
 
   static String Upload = "http://upload.tuuz.cc:81/upfull?token=" + Upload_token;
   static String Upload_token = "gobotq";
